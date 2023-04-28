765bfc74df6b47e55e9cd6b4cd74c26c2057f00a
Jackson Tai <jacksontai.dev@gmail.com>
2023-04-28 21:44:11 +0800

    Add scanning loading animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('test')
+const scanButton = document.getElementById('scan-button');
+scanButton.addEventListener('click', () => {
+  Swal.fire({
+    title: 'Scanning in progress...',
+    timer: 4000,
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading()
+      const b = Swal.getHtmlContainer().querySelector('b')
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+        
+    }
+  });
+});
